4d9ce19db69e253763195167bde1c863f17d81ab
Mondo <568197478@qq.com>
2018-09-04 18:45:16 +0800

    滚动加载更多

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,17 +5,49 @@ class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      article: []
+      article: [],
+      pageNo: 1,
+      count: 0
     }
   }
-  async componentWillMount() {
-    let list = await axios.get('/api/classes/article?pageSize=9&pageNo=1');
-    this.setState({ 
-      article: list.results
-    });
+  
+  handleScroll = () => {
+    const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    const clientHeight = document.documentElement.clientHeight;
+    const scrollHeight = document.documentElement.scrollHeight;
+    const isBottom = (scrollHeight - (scrollTop + clientHeight));
+    let hasNoMore = this.state.article.length < this.state.count;
+    if (hasNoMore && scrollTop && !isBottom) {
+      this.getList();
+    }
   }
+
+  componentDidMount() {
+    this.scrollEvent = window.addEventListener('scroll', this.handleScroll);
+    this.handleScroll();
+  } 
+
+  componentWillMount() {
+    this.getList();
+  }
+
+  async getList() {
+    let list = await axios.get(`/api/classes/article?pageSize=10&pageNo=${this.state.pageNo}`);
+    let article = [...this.state.article, ...list.results];
+    let pageNo = this.state.pageNo;
+    pageNo++;
+    this.setState({ article, pageNo, count: list.count });   
+  }
+
+  componentWillUnmount() {
+    if (this.scrollEvent) {
+      this.scrollEvent.remove();
+    }
+  }
+
   render () {
     const { article } = this.state;
+    console.log(article)
     return (
       <div className="index"> 
         { article.map((val) => {
